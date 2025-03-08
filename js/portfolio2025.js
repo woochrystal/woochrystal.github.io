859e8ec906b8c4156e26ad410072c96d905e8845
DESKTOP-H208UA1\82104 <woochryfront@gmail.com>
2025-03-08 23:17:31 +0900

    포폴 제목 안사라짐 이슈

--- js/portfolio2025.js
+++ js/portfolio2025.js
@@ -1,23 +1,28 @@
 // 202503 프론트 포폴 작업파일
+window.onload = function() {//다 로드 된 후 실행
 
 
 //프로젝트 제목 마우스 띄우기
 //퍼블리싱 제목명
 var pub_tit = [
-    "아디다스 이지부스트 리뉴얼 사이트",
-    "GTA5 원프레임 반응형 사이트",
+    "이로케 가구",
+    "신라호텔 서울 리뉴얼",
+    "조말론 앱",
+    "아디다스 이지부스트 리뉴얼",
+    "GTA5",
     "옹성우 홍보 사이트 부트스트랩",
-    "우수정 포트폴리오 반응형 사이트",
+    "우수정 포트폴리오",
 ];
 //퍼블리싱 컨텐츠
 var pub_cons = document.querySelectorAll('.web-content .content');
+// console.log(Array.from(pub_cons))
 
 //제목박스
 var mouse_box = document.querySelector('.mouse');
 
 pub_cons.forEach(function(pub, index){
+    console.log(pub)
     pub.addEventListener('mouseover',function(){//마우스 올렸을때 이벤트
-        console.log(index)
 
         document.addEventListener('mousemove', function (e){//mouseover 이벤트가 발생했을때
             var mouseX = e.pageX;//마우스 수평 좌표
@@ -33,6 +38,18 @@ pub_cons.forEach(function(pub, index){
     
     pub.addEventListener('mouseout',function(){//마우스 나갔을때
         mouse_box.style.display = 'none';
+        console.log(mouse_box.style.display)
     });//mouseover end
 })
 
+document.addEventListener('mousemove', function(e) {
+    var mouseBox = document.querySelector('.mouse');
+
+    // 마우스가 .content 영역 밖으로 벗어났을 때, .mouse 숨기기
+    if (!document.querySelector('.web-content').contains(e.target)) {
+        mouseBox.style.display = 'none'; // 마우스가 .content 영역을 벗어났다면 숨기기
+    }
+});
+
+
+};
